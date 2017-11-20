cbccd417b78a2b227e2105cf94e9e61ab8cd69b2
Yvonne Shih <tailcloud14916@gmail.com>
2017-11-20 10:20:11 +0800

    appid/pwd

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const restify = require('restify');
 const builder = require('botbuilder');
 
-const MICROSOFT_APP_ID = '';
-const MICROSOFT_APP_PASSWORD = '';
+const MICROSOFT_APP_ID = 'f72a999a-24a2-4154-b6de-835aec6adbaf';
+const MICROSOFT_APP_PASSWORD = 'Mu4H62kvb9w3Gg3By40p71Y';
 
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
